783747723ddc61049305ab12e16e3a0874ffa1e3
xyz <81219723@qq.com>
2023-02-21 14:52:45 +0800

    add 03_JSON

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.post('/server',(request, response)=>{
     response.send('HELLO AJAX POST -2')
 })
 
+app.all('/json-server', (request, response)=>{
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Headers', '*')
+    const data = {
+        name:'json obj'
+    }
+    response.send(JSON.stringify(data))
+})
+
 app.listen(8000, ()=>{
     console.log('服务已启动， 8000端口监听中....')
 })

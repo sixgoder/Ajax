7a6c2cd4fa91e4aded3a0fbbccc25616fc3a375c
xyz <81219723@qq.com>
2023-02-22 13:58:57 +0800

    add 05_jQuery

--- server.js
+++ server.js
@@ -21,6 +21,18 @@ app.all('/json-server', (request, response)=>{
     response.send(JSON.stringify(data))
 })
 
+app.get('/delay',(request, response)=>{
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    setTimeout(() => {
+        response.send('Delay Response')
+    }, 3000);
+})
+
+app.all('/jquery-server',(request, response)=>{
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.send('jQuery ajax')
+})
+
 app.listen(8000, ()=>{
     console.log('服务已启动， 8000端口监听中....')
 })
